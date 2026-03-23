1cffc9af0b811507fc9ab9680f609351e4eb9241
Krittaphat  Ngampriam <krittaphatng@gmail.com>
2026-03-23 09:42:33 +0700

    feat: remove login package and related authentication logic

--- login/dto.go
+++
@@ -1,9 +0,0 @@
-package login
-
-type LoginRequest struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-type LoginResponse struct {
-    Token string `json:"token"`
-}
